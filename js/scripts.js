9c168b13f00894b5734d991ef6c5b17d951a2b0a
Alix Alto <alix.t.alto@gmail.com>
2022-11-20 08:56:37 -0500

    Re-add JS

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function roboger(n) {
+  let result = 0
+  for (let i = 0; i <= n; i++) {
+  
+    if (i.toString().includes("3")) {
+    result[i] = "Won't you be my neighbor?";
+  } else if (i.toString().includes("2")) {
+    result[i] = "Boop!";
+  } else if (i.toString().includes("!")) {
+    result [i] = "Beep!";
+  }
+ }
+ return result
+}
